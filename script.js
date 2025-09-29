a5db458f218d42927cc4af763ddb185a05a28b17
JossyClover <Josselynkastro@gmail.com>
2025-09-28 18:39:37 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const messages = [
+    "Te quiero mucho aury",
+    "Gracias por siempre apoyarme",
+    "Te voy a extrañar mucho",
+    "Mucha suerte en tu viaje",
+    "cumple todos tus sueños",
+    "Que Dios te bendiga grandemente"
+];
